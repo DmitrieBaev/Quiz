f7c87af01899aaacf4c9ba01f90af32aefb4b123
quotoms <azo.cw@yandex.ru>
2022-06-25 22:25:05 +0300

    Добавлен просмотр ВОПРОСНИКОВ

--- Fortech/testing_app/src/views/HomeView.vue
+++ Fortech/testing_app/src/views/HomeView.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="home">
+  <h1>HOME</h1>
+  <!-- <p>{{ questionary }}</p> -->
+
+  <div v-for="currency in questionary" class="currency">
+    <router-link :to="{name: 'questionary-by-id', params: {id: currency.pk}}">{{ currency.caption }}</router-link>
+  </div>
+
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  data() {
+    return { questionary: null }
+  },
+  mounted() {
+    axios
+        .get('/api/v1/questionary/')
+        .then(response => (this.questionary = response.data.results))
+  }
 }
 </script>
